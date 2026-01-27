2dfc53776a6fb501abd3cb04c02dfaaec08878ce
Luis SebastianHuerta <luissebastianhuerta@gmail.com>
2026-01-27 09:13:07 +0100

    Configure Firebase Realtime Database
    - Add Firebase SDK config with project credentials
    - Add database security rules
    - Add Firebase project configuration

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -22,13 +22,13 @@
  */
 
 const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_PROYECTO.firebaseapp.com",
-    databaseURL: "https://TU_PROYECTO-default-rtdb.firebaseio.com",
-    projectId: "TU_PROYECTO",
-    storageBucket: "TU_PROYECTO.appspot.com",
-    messagingSenderId: "TU_SENDER_ID",
-    appId: "TU_APP_ID"
+    apiKey: "AIzaSyDbtMcIULW0hh5L0qXJHTnJIRD8K-xlRAg",
+    authDomain: "murder-my-friends.firebaseapp.com",
+    databaseURL: "https://murder-my-friends-default-rtdb.firebaseio.com",
+    projectId: "murder-my-friends",
+    storageBucket: "murder-my-friends.firebasestorage.app",
+    messagingSenderId: "504049550535",
+    appId: "1:504049550535:web:1d9e9ac7049028f9c4eae2"
 };
 
 // Verificar si Firebase está configurado
